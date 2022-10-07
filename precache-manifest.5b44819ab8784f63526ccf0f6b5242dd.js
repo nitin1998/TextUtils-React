b0085f70bd98367f6eb8fee7215952d9c3e613d9
nitin1998 <nitin.pahwa21@gmail.com>
2022-10-08 00:48:49 +0530

    Updates

---
+++ precache-manifest.5b44819ab8784f63526ccf0f6b5242dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1e7c8980f6cdabeabe4",
+    "url": "/TextUtils-React/static/css/main.1c3db5f6.chunk.css"
+  },
+  {
+    "revision": "a1e7c8980f6cdabeabe4",
+    "url": "/TextUtils-React/static/js/main.a1e7c898.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "4b34c9a858fd5a531cd218984375186f",
+    "url": "/TextUtils-React/index.html"
+  }
+];
